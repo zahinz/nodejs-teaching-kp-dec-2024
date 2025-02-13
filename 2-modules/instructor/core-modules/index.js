aeeba34d18a9028421455445b82c9bb6081d3006
zahinz <dev.huxnet@gmail.com>
2025-02-13 22:46:50 +0800

    finish code along

--- 2-modules/instructor/core-modules/index.js
+++ 2-modules/instructor/core-modules/index.js
@@ -1,17 +1,22 @@
 // console.log("core modules");
 const fs = require("node:fs");
 const path = require("node:path");
+const crypto = require("node:crypto");
 
+// UUID is a universally unique identifier (UUID) is a 128-bit number used to identify information in computer systems.
+const uuid = crypto.randomUUID();
+console.log(uuid);
 // EXAMPLE 1
 const contentToWrite = "Hello, Data!";
+const fileName = `${uuid}.txt`;
 // this will work only on mac but not on windows
 // const fileName = "data/thisisdata.txt";
 // this will work on all OS due to path.join
-const fileName = path.join("data", "thisisdata-path.txt");
+// const fileName = path.join("data", "thisisdata-path.txt");
 
-fs.writeFile(fileName, contentToWrite, function (err) {
+fs.writeFile(fileName, contentToWrite, function (error) {
   if (err) {
-    console.log(err);
+    console.log(error);
   } else {
     console.log("File written successfully");
   }
