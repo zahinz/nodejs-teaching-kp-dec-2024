2ea5d2c1aae9bac70266647f7a52623a6401998f
zahinz <dev.huxnet@gmail.com>
2025-02-18 22:01:41 +0800

    finish http code module

--- 3-http/instructor/node-http/index.js
+++ 3-http/instructor/node-http/index.js
@@ -1,14 +1,31 @@
 const http = require("node:http");
+const fs = require("node:fs");
+const path = require("node:path");
 
 const server = http.createServer(function (req, res) {
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(
-    JSON.stringify({
-      data: "Hello from node js http server!",
-    })
-  );
-
   // create a new /contact route and respond with a JSON object
+  const url = req.url;
+
+  if (url === "/") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(
+      JSON.stringify({
+        data: "Hello from node js http server!",
+      })
+    );
+  } else if (url === "/contact") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(
+      JSON.stringify({
+        data: "Contact data",
+      })
+    );
+  } else {
+    const pathNotFoundPage = path.join("pages", "404.html");
+    const notFoundPage = fs.readFileSync(pathNotFoundPage, "utf-8");
+    res.writeHead(404, { "Content-Type": "text/html; charset=utf-8" });
+    res.end(notFoundPage);
+  }
 });
 
 // listen for requests
