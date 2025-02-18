8a270b20df68f19daf7a515c11e6195c7cc17879
zahinz <dev.huxnet@gmail.com>
2025-02-18 22:10:16 +0800

    add visitor count

--- 3-http/instructor/node-http/index.js
+++ 3-http/instructor/node-http/index.js
@@ -2,24 +2,27 @@ const http = require("node:http");
 const fs = require("node:fs");
 const path = require("node:path");
 
+let visitorCount = 0;
 const server = http.createServer(function (req, res) {
+  console.log(`Current visitor count: ${visitorCount}`);
+  console.log(`You vsitor number ${visitorCount + 1}`);
+
+  const ipAddress = req.connection.remoteAddress;
+  console.log(`Visitor IP Address: ${ipAddress}`);
   // create a new /contact route and respond with a JSON object
   const url = req.url;
-
   if (url === "/") {
+    visitorCount++;
     res.writeHead(200, { "Content-Type": "application/json" });
     res.end(
       JSON.stringify({
         data: "Hello from node js http server!",
       })
     );
-  } else if (url === "/contact") {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(
-      JSON.stringify({
-        data: "Contact data",
-      })
-    );
+  } else if (url === "/whatsapp") {
+    visitorCount++;
+    res.writeHead(301, { Location: "https://wa.me/60196974200" });
+    res.end();
   } else {
     const pathNotFoundPage = path.join("pages", "404.html");
     const notFoundPage = fs.readFileSync(pathNotFoundPage, "utf-8");
