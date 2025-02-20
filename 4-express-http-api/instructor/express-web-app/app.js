2c616ef1b6b8d3ad89459868d64190d1d1664ab4
zahinz <dev.huxnet@gmail.com>
2025-02-20 21:57:12 +0800

    finish routes

--- 4-express-http-api/instructor/express-web-app/app.js
+++ 4-express-http-api/instructor/express-web-app/app.js
@@ -5,6 +5,7 @@ import fs from "node:fs";
 const PORT = 3000;
 const app = express();
 
+// static routes
 app.get("/", function (req, res) {
   const homeHtmlPath = path.resolve("pages", "index.html");
   const homeHtml = fs.readFileSync(homeHtmlPath, "utf-8");
@@ -13,14 +14,24 @@ app.get("/", function (req, res) {
   res.end();
 });
 
+// dynamic routes
 // routes parameters
 app.get("/users/:username", function (req, res) {
-  console.log(req.params);
+  const username = req.params.username;
+  const formatUsername = username.replaceAll("-", " ");
+  // render the user page by using the parameter from the route
   const userHtmlPath = path.resolve("pages", "user.html");
   const userHtml = fs.readFileSync(userHtmlPath, "utf-8");
+  const updatedHtml = userHtml.replace("[[USERNAME]]", formatUsername);
   res.setHeader("Content-Type", "text/html");
-  res.send(userHtml);
+  res.send(updatedHtml);
   res.end();
+});
+
+// query parameters
+app.get("/search", function (req, res) {
+  const query = req.query;
+  res.send(`<pre>${JSON.stringify(query, null, 2)}</pre>`);
 });
 
 app.listen(PORT, () => {
