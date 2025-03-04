e53ef8eb820630ea871627fc93718c972d2042c4
zahinz <dev.huxnet@gmail.com>
2025-03-04 23:12:33 +0800

    add sql file

---
+++ 6-db-design/ddl.sql
@@ -0,0 +1,23 @@
+-- Create Database
+CREATE DATABASE "bitly-clone";
+
+-- delete database
+-- warning this will delete all data in the database and cannot be undone
+DROP DATABASE "bitly-clone";
+
+-- Create User table
+CREATE TABLE "Users" (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(120),
+    password VARCHAR(120),
+    username VARCHAR(120)
+);
+
+-- Create Link table
+CREATE TABLE "Links" (
+    id SERIAL PRIMARY KEY,
+    actual_link TEXT,
+    shortened_link VARCHAR(120),
+    created_by INTEGER,
+    FOREIGN KEY (created_by) REFERENCES "Users"(id)
+);
